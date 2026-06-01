44ff11dfaf5e445ce2f50af65a2ffbe9e2f42947
lilianaw13 <istrliliana3433@gmail.com>
2026-06-02 02:03:45 +0300

    updates

--- src/main/java/expenseTracker/MainUI.java
+++ src/main/java/expenseTracker/MainUI.java
@@ -3,8 +3,10 @@ package main.java.expenseTracker;
 import main.java.expenseTracker.UI.ExpenseTrackerUI;
 import javafx.application.Application;
 
+import static javafx.application.Application.*;
+
 public class MainUI {
     public static void main(String[] args) {
-        Application.launch(ExpenseTrackerUI.class, args);
+        launch(ExpenseTrackerUI.class, args);
     }
 }
